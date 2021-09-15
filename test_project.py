fcfa241ea7df8612d0e3b08a25e108fcacaf6ea2
Yiru Xiong <yiruxion@andrew.cmu.edu>
2021-09-15 03:20:55 -0400

    initializing repository

---
+++ test_project.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+a test repository
+"""
+import numpy as np
+ import itertools
+ combination=[]
+ for u, v in itertools.product(tags, tags):
+     combination.append((u,v))
+     print("hello world!")
